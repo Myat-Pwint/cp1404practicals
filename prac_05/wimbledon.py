41261dd9630e59ad9b40fd0dac8610558d0a88ff
Myat-Pwint <myatpwintphyu@my.jcu.edu.au>
2023-04-23 03:10:55 +0800

    complete prac 5 wimbledon exercise

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,49 @@
+import csv
+
+
+def main():
+    champion_data = read_csv_file("wimbledon.csv")
+    champions = get_champions(champion_data)
+    display_champions(champions)
+    countries = get_countries(champion_data)
+    display_countries(countries)
+
+
+def read_csv_file(file_name):
+    with open(file_name, "r", encoding="utf-8-sig") as in_file:
+        reader = csv.reader(in_file)
+        next(reader)
+        return [row for row in reader]
+
+
+def get_champions(champion_data):
+    champions = {}
+    for row in champion_data:
+        champion = row[2]
+        if champion in champions:
+            champions[champion] += 1
+        else:
+            champions[champion] = 1
+    return champions
+
+
+def get_countries(champion_data):
+    countries = set()
+    for row in champion_data:
+        country = row[1]
+        countries.add(country)
+    return sorted(countries)
+
+
+def display_champions(champions):
+    print("Wimbledon Champions:")
+    for champion, count in sorted(champions.items()):
+        print(f"{champion} {count}")
+
+
+def display_countries(countries):
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(countries))
+
+
+main()
