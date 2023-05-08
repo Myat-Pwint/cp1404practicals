72fd692b0643739cb161eb561d081a3cf20383e8
Myat-Pwint <myatpwintphyu@my.jcu.edu.au>
2023-05-08 14:56:57 +0800

    complete practical 8 exercises

--- prac_08/test.py
+++ prac_08/test.py
@@ -1,11 +1,1 @@
-from kivy.app import App
-from kivy.app import Widget
-
-
-class HelloWorld(App):
-    def build(self):
-        self.root = Widget()
-        return self.root
-
-
-HelloWorld().run()
+test
