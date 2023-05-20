7137525b993dbafba1400aee54bf0da5059f98b7
Myat-Pwint <myatpwintphyu@my.jcu.edu.au>
2023-05-21 02:32:50 +0800

    practical 09 completed silver service taxi exercise

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,8 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+my_taxi = SilverServiceTaxi("Hummer", 200, 2)
+
+# Drive the taxi for 18 km
+my_taxi.drive(18)
+fare = my_taxi.get_fare()
+print("Fare: ${:.2f}".format(fare))
